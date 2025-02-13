daeaaeb0c2b3bfe28f5bed77f705a6f005d55d36
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 11:10:11 +0100

    second2

---
+++ CommonJS/index.js
@@ -0,0 +1,16 @@
+// Destructuring
+
+const { SOMME , PRODUIT } = require('./mathsLib.cjs');
+
+console.log(SOMME(4,7));
+console.log(PRODUIT(4,2));
+
+const nbr1 = 215;
+const nbr2 = 2215;
+
+
+console.log(`${nbr1} + ${nbr2} = ${SOMME(nbr1,nbr2)}`);
+console.log(`${nbr1} * ${nbr2} = ${PRODUIT(nbr1,nbr2)}`);
+
+
+
