d2eec1b92ac1c8f3eed52c4780de35495a5b69be
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 10:42:57 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log('Introduction a nodeJS');
+
 const mathsLib = require('./mathsLib');
 
 let somme = mathsLib.SOMME(4,7);
@@ -7,5 +9,11 @@ let produit = mathsLib.PRODUIT(4,2)
 console.log(somme);
 console.log(produit);
 
-console.log('Introduction a nodeJS');
+const nbr1 = 215;
+const nbr2 = 2215;
+
+console.log(`${nbr1} + ${nbr2} = ${mathsLib.SOMME(nbr1,nbr2)}`);
+console.log(`${nbr1} * ${nbr2} = ${mathsLib.PRODUIT(nbr1,nbr2)}`);
+
+
 
