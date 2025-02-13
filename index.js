753aab6645a1d5c83298bd31f96852ea37f7327c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 10:39:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const mathsLib = require('./mathsLib');
+
+let somme = mathsLib.SOMME(4,7);
+
+let produit = mathsLib.PRODUIT(4,2)
+
+console.log(somme);
+console.log(produit);
+
+console.log('Introduction a nodeJS');
+
---
+++ mathsLib.js
@@ -0,0 +1,8 @@
+const sum = (a,b) => a + b;
+const prod = (a,b) => a * b;
+
+module.exports = {
+    SOMME : sum,
+    PRODUIT: prod,
+}
+// module.exports =  sum, prod ;
