be9588b46e6d6e02542f0577b6765d1ce569bcd3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 10:54:21 +0100

    second

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ console.log('Introduction a nodeJS');
 // const mathsLib = require('./mathsLib');
 
 // let somme = mathsLib.SOMME(4,7);
-
 // let produit = mathsLib.PRODUIT(4,2)
+// console.log(somme;
+// console.log(produit);
+
+// console.log(`${nbr1} + ${nbr2} = ${mathsLib.SOMME(nbr1,nbr2)}`);
+// console.log(`${nbr1} * ${nbr2} = ${mathsLib.PRODUIT(nbr1,nbr2)}`);
+
+// Destructuring
+
 let { somme , produit } = require('./mathsLib');
 
 console.log(somme(4,7));
@@ -13,8 +20,6 @@ console.log(produit(4,2));
 const nbr1 = 215;
 const nbr2 = 2215;
 
-// console.log(`${nbr1} + ${nbr2} = ${mathsLib.SOMME(nbr1,nbr2)}`);
-// console.log(`${nbr1} * ${nbr2} = ${mathsLib.PRODUIT(nbr1,nbr2)}`);
 
 console.log(`${nbr1} + ${nbr2} = ${somme(nbr1,nbr2)}`);
 console.log(`${nbr1} * ${nbr2} = ${produit(nbr1,nbr2)}`);
