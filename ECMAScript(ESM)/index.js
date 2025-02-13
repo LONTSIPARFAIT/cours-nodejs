596ad4183d53914aa20af566a8ba1ea827172905
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 11:57:37 +0100

    second2

--- ECMAScript(ESM)/index.js
+++ ECMAScript(ESM)/index.js
@@ -1,16 +1,16 @@
 // Destructuring
 
-const { SOMME , PRODUIT } = require('./mathsLib.mjs');
+import { sum , prod } from "./mathsLib.mjs" ;
 
-console.log(SOMME(4,7));
-console.log(PRODUIT(4,2));
+console.log(sum(4,7));
+console.log(prod(4,2));
 
 const nbr1 = 215;
 const nbr2 = 2215;
 
 
-console.log(`${nbr1} + ${nbr2} = ${SOMME(nbr1,nbr2)}`);
-console.log(`${nbr1} * ${nbr2} = ${PRODUIT(nbr1,nbr2)}`);
+console.log(`${nbr1} + ${nbr2} = ${sum(nbr1,nbr2)}`);
+console.log(`${nbr1} * ${nbr2} = ${prod(nbr1,nbr2)}`);
 
 
 
--- ECMAScript(ESM)/mathsLib.mjs
+++ ECMAScript(ESM)/mathsLib.mjs
@@ -2,5 +2,3 @@ const sum = (a,b) => a + b;
 const prod = (a,b) => a * b;
 
 export  { sum, prod }
-
-// module.exports =  sum, prod ;
